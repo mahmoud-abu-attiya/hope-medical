4f1957ab6e9c9149995d4b38b12b8f54e87a17fe
Yousef <yousefghazal2412@gmail.com>
2022-11-25 14:13:43 +0200

    coming soon

--- main/admin.py
+++ main/admin.py
@@ -1,4 +1,28 @@
 from django.contrib import admin
-from .models import *
-# Register your models here.
-admin.site.register()
+from .models import Service, ServiceType, Doctor, AppointmentForm, ClientForm 
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = []
+    list_filter = []
+    search_fields = []
+
+@admin.register(ServiceType)
+class ServiceTypeAdmin(admin.ModelAdmin):
+    list_display = []
+    list_filter = []
+    search_fields = []
+@admin.register(Doctor)
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = []
+    list_filter = []
+    search_fields = []
+@admin.register(AppointmentForm)
+class AppointmentFormAdmin(admin.ModelAdmin):
+    list_display = []
+    list_filter = []
+    search_fields = []
+@admin.register(ClientForm)
+class ClientFormAdmin(admin.ModelAdmin):
+    list_display = []
+    list_filter = []
+    search_fields = []
--- medical/urls.py
+++ medical/urls.py
@@ -20,6 +20,8 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('main.urls')),
+
     re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
     re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
 
